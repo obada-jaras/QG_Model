443b60c8e5b314073c43a72d4a3d28e5e7ea6281
OBADA TAHAYNA <obada.jaras@yahoo.com>
2023-06-19 12:46:59 +0300

    add squad_converter_t5.py

---
+++ squad_converter_t5.py
@@ -0,0 +1,34 @@
+import argparse
+import json
+
+def read_squad_file(input_file):
+    with open(input_file, 'r', encoding='utf-8') as f:
+        data = json.load(f)
+    return data
+
+def write_simplified_file(output_file, simplified_data):
+    with open(output_file, 'w', encoding='utf-8') as f:
+        json.dump(simplified_data, f, indent=2)
+
+def convert_squad_to_simplified(input_file, output_file):
+    squad_data = read_squad_file(input_file)
+    simplified_data = []
+
+    for article in squad_data['data']:
+        for paragraph in article['paragraphs']:
+            context = paragraph['context']
+            for qa in paragraph['qas']:
+                question = qa['question']
+                answer = qa['answers'][0]['text'] if qa['answers'] else ''
+                simplified_data.append({"context": context, "question": question, "answer": answer})
+
+    write_simplified_file(output_file, simplified_data)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Convert SQuAD-format JSON file to simplified JSON file with context, questions, and answers.")
+    parser.add_argument('--input_file', type=str, default="merged_dataset.json", help="Path to the input SQuAD-format JSON file.")
+    parser.add_argument('--output_file', type=str, default="simplified_dataset.json", help="Path to the output simplified JSON file.")
+
+    args = parser.parse_args()
+
+    convert_squad_to_simplified(args.input_file, args.output_file)
